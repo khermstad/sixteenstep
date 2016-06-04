3d16d01e1fa342958d68760469ba06e465fbdec2
Kris Hermstad <krishermstad@gmail.com>
2016-06-04 19:16:10 -0400

    "changed 'node' to 'beat' in reference to functions accessing a nodeState. the beat refers to [0-15] of the possible nodes in each track"

--- sixteenstep.js
+++ sixteenstep.js
@@ -20,18 +20,19 @@ var currentlyPlaying = false;
 // nodeState holds the current state (0 for off, 1 for on) of each node on the sequencer.
 // [0-127] - 8 tracks, 16 nodes each - 1 node for each 16th note in a single 4/4 measure.
 
-var nodeState = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,    // track1: (0-15)
-                 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,    // track2: (0-15) + 16
-                 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,    // track3: (0-15) + 32
-                 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,    // track4: (0-15) + 48
-                 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,    // track5: (0-15) + 64
-                 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,    // track6: (0-15) + 80
-                 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,    // track7: (0-15) + 96
-                 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];   // track8: (0-15) + 112
+var nodeState = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],    // track0: nodeState[0][0-15]
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],    // track1: nodeState[1][0-15]
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],    // track2: nodeState[2][0-15]
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],    // track3: nodeState[3][0-15]
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],    // track4: nodeState[4][0-15]
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],    // track5: nodeState[5][0-15]
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],    // track6: nodeState[6][0-15]
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]];   // track7: nodeState[7][0-15]
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////    MAIN LOOP ///////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 /**
  * masterLoop: main loop for the sixteen step sequencer.
  * loop interval = "16n" - the loop iterates through each 16th note of 4/4 bar.
@@ -40,6 +41,7 @@ var nodeState = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,    // track1: (
  */
 
 var masterLoop = new Tone.Loop(function(time) {
+
 
     console.log(currentBeat);
     updateCurrentBeat();
@@ -53,31 +55,55 @@ var masterLoop = new Tone.Loop(function(time) {
 /**
  * Samplers:
  * Each sampler holds "sets" of drum samples.  A.1 = first set, A.2 = second set, etc;
- *
+ * A.1: Roland TR 808 - samplebank/roland_TR_808/
  * @type {Tone}
  */
 
 var t1sampler = new Tone.Sampler({
     A : {
-        1 : "./roland_TR_808/Bassdrum-01.wav",
+        1 : "./samplebank/roland_TR_808/Bassdrum-01.wav",
     }
 }).toMaster();
 
 var t2sampler = new Tone.Sampler({
     A : {
-        1 : "./roland_TR_808/Clap.wav"
+        1 : "./samplebank/roland_TR_808/Clap.wav"
     }
 }).toMaster();
 
 var t3sampler = new Tone.Sampler({
     A : {
-        1 : "./roland_TR_808/Snaredrum.wav"
+        1 : "./samplebank/roland_TR_808/Snaredrum.wav"
     }
 }).toMaster();
 
 var t4sampler = new Tone.Sampler({
     A : {
-        1 : "./roland_TR_808/Snaredrum.wav"
+        1 : "./samplebank/roland_TR_808/HatOpen.wav"
+    }
+}).toMaster();
+
+var t5sampler = new Tone.Sampler({
+    A : {
+        1 : "./samplebank/roland_TR_808/HatClosed.wav",
+    }
+}).toMaster();
+
+var t6sampler = new Tone.Sampler({
+    A : {
+        1 : "./samplebank/roland_TR_808/TomH.wav"
+    }
+}).toMaster();
+
+var t7sampler = new Tone.Sampler({
+    A : {
+        1 : "./samplebank/roland_TR_808/TomL.wav"
+    }
+}).toMaster();
+
+var t8sampler = new Tone.Sampler({
+    A : {
+        1 : "./samplebank/roland_TR_808/TomM.wav"
     }
 }).toMaster();
 
@@ -105,27 +131,28 @@ function togglePlayState(){
     }
 }
 
-function toggleNodeState(node){
-    if (getNodeState(node) == 0){
-        turnOnNode(node);
+function toggleNodeState(track, beat){
+    if (getNodeState(track, beat) == 0){
+        turnOnNode(track, beat);
     }
     else {
-        turnOffNode(node);
+        turnOffNode(track, beat);
     }
 }
 
-function turnOnNode(node){
-    setNodeState(node, 1);
+function turnOnNode(track, beat){
+    setNodeState(track, beat, 1);
 }
 
-function turnOffNode(node){
-    setNodeState(node, 0);
+function turnOffNode(track, beat){
+    setNodeState(track, beat, 0);
 }
 
-function getNodeState(node){
-    return nodeState[node];
+function getNodeState(track, beat){
+    console.log(nodeState[track][beat]);
+    return nodeState[track][beat];
 }
 
-function setNodeState(node, newNodeState){
-    nodeState[node] = newNodeState;
+function setNodeState(track, beat, newNodeState){
+    nodeState[track][beat] = newNodeState;
 }
