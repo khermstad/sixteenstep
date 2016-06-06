2f9c0e7774d7280e0fcc4fbbd6e6d96a69fb75fe
Kris Hermstad <krishermstad@gmail.com>
2016-06-05 21:08:49 -0400

    "created ui.js to separate the sequencers logic from its UI functions"

---
+++ js/ui.js
@@ -0,0 +1,69 @@
+// ALL UI JAVASCRIPT GOES HERE
+
+// setMetronome();
+function setMetronome() {
+    if (currentBeat == 0) {
+        document.getElementById("m15").style.backgroundColor = "lightpink";
+        document.getElementById("m0").style.backgroundColor = "red";
+    }
+    if (currentBeat == 1) {
+        document.getElementById("m0").style.backgroundColor = "lightpink";
+        document.getElementById("m1").style.backgroundColor = "red";
+    }
+    if (currentBeat == 2) {
+        document.getElementById("m1").style.backgroundColor = "lightpink";
+        document.getElementById("m2").style.backgroundColor = "red";
+    }
+    if(currentBeat == 3) {
+        document.getElementById("m2").style.backgroundColor = "lightpink";
+        document.getElementById("m3").style.backgroundColor = "red";
+    }
+    if (currentBeat == 4) {
+        document.getElementById("m3").style.backgroundColor = "lightpink";
+        document.getElementById("m4").style.backgroundColor = "red";
+    }
+    if (currentBeat == 5){
+        document.getElementById("m4").style.backgroundColor = "lightpink";
+        document.getElementById("m5").style.backgroundColor = "red";
+    }
+    if (currentBeat == 6){
+        document.getElementById("m5").style.backgroundColor = "lightpink";
+        document.getElementById("m6").style.backgroundColor = "red";
+    }
+    if (currentBeat == 7){
+        document.getElementById("m6").style.backgroundColor = "lightpink";
+        document.getElementById("m7").style.backgroundColor = "red";
+    }
+    if (currentBeat == 8){
+        document.getElementById("m7").style.backgroundColor = "lightpink";
+        document.getElementById("m8").style.backgroundColor = "red";
+    }
+    if (currentBeat == 9){
+        document.getElementById("m8").style.backgroundColor = "lightpink";
+        document.getElementById("m9").style.backgroundColor = "red";
+    }
+    if (currentBeat == 10){
+        document.getElementById("m9").style.backgroundColor = "lightpink";
+        document.getElementById("m10").style.backgroundColor = "red";
+    }
+    if (currentBeat == 11){
+        document.getElementById("m10").style.backgroundColor = "lightpink";
+        document.getElementById("m11").style.backgroundColor = "red";
+    }
+    if (currentBeat == 12){
+        document.getElementById("m11").style.backgroundColor = "lightpink";
+        document.getElementById("m12").style.backgroundColor = "red";
+    }
+    if (currentBeat == 13){
+        document.getElementById("m12").style.backgroundColor = "lightpink";
+        document.getElementById("m13").style.backgroundColor = "red";
+    }
+    if (currentBeat == 14){
+        document.getElementById("m13").style.backgroundColor = "lightpink";
+        document.getElementById("m14").style.backgroundColor = "red";
+    }
+    if (currentBeat == 15){
+        document.getElementById("m14").style.backgroundColor = "lightpink";
+        document.getElementById("m15").style.backgroundColor = "red";
+    }
+}
